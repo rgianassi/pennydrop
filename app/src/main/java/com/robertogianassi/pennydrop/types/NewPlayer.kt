0e70dd79f44542f1abdccf323f91d76978a93f27
Roberto Gianassi <roberto.gianassi+github.com@gmail.com>
2021-12-29 10:03:04 +0100

    feat: Add PickPlayersViewModel

---
+++ app/src/main/java/com/robertogianassi/pennydrop/types/NewPlayer.kt
@@ -0,0 +1,11 @@
+package com.robertogianassi.pennydrop.types
+
+import androidx.databinding.ObservableBoolean
+
+data class NewPlayer(
+    var playerName: String = "",
+    val isHuman: ObservableBoolean = ObservableBoolean(true),
+    val canBeRemoved: Boolean = true,
+    val canBeToggled: Boolean = true,
+    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved)
+)
---
+++ app/src/main/java/com/robertogianassi/pennydrop/viewmodels/PickPlayersViewModel.kt
@@ -0,0 +1,16 @@
+package com.robertogianassi.pennydrop.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.robertogianassi.pennydrop.types.NewPlayer
+
+class PickPlayersViewModel : ViewModel() {
+    val players = MutableLiveData<List<NewPlayer>>().apply {
+        this.value = (1..6).map {
+            NewPlayer(
+                canBeRemoved = it > 2,
+                canBeToggled = it > 1
+            )
+        }
+    }
+}
